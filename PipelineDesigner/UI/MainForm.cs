be18de9efb28b24b62b4c65d0d871d4b7eac9401
Lavliet-PC\Lavliet <mantigor2@gmail.com>
2025-05-14 15:34:08 +0500

    Минимальный функционал требует тестирования и доработки функционала

--- PipelineDesigner/UI/MainForm.cs
+++ PipelineDesigner/UI/MainForm.cs
@@ -3,7 +3,9 @@ using PipelineDesigner.Models;
 using PipelineDesigner.Services;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -13,29 +15,45 @@ namespace PipelineDesigner
     {
         private PipelineRepository _repo;
         private string connStr;
-        //private string connStr = "Data Source=Database\\pipeline.db;Version=3;";
         private List<Node> _nodes;
 
         public MainForm()
         {
             InitializeComponent();
+
             string dbFolder = System.IO.Path.Combine(Application.StartupPath, "Database");
             if (!System.IO.Directory.Exists(dbFolder))
                 System.IO.Directory.CreateDirectory(dbFolder);
 
             connStr = $"Data Source={System.IO.Path.Combine(dbFolder, "pipeline.db")};Version=3;";
-
             _repo = new PipelineRepository(connStr);
             _nodes = new List<Node>();
-            DatabaseInitializer.Initialize(connStr);
+            if (!System.IO.File.Exists(Path.Combine(dbFolder, "pipeline.db")))
+            {
+                DatabaseInitializer.Initialize(connStr);
+            }
+            //MessageBox.Show(connStr);
             LoadNodes();
         }
 
         private void LoadNodes()
         {
             _nodes = _repo.GetNodes();
-            dataGridView1.DataSource = null;
-            dataGridView1.DataSource = _nodes;
+
+            var table = new DataTable();
+            table.Columns.Add("№", typeof(int));
+            table.Columns.Add("X", typeof(double));
+            table.Columns.Add("Y", typeof(double));
+
+            foreach (var group in _nodes.GroupBy(n => n.PipelineId))
+            {
+                foreach (var node in group)
+                {
+                    table.Rows.Add(group.Key, node.X, node.Y);
+                }
+            }
+
+            dataGridView1.DataSource = table;
         }
 
         private void btnDraw_Click(object sender, EventArgs e)
@@ -46,12 +64,16 @@ namespace PipelineDesigner
         private void btnCheck_Click(object sender, EventArgs e)
         {
             var collisions = CollisionChecker.FindCollisions(_nodes);
+            var collisionSet = collisions.Select(c => (c.X, c.Y)).ToHashSet();
+
             foreach (DataGridViewRow row in dataGridView1.Rows)
             {
-                var id = (int)row.Cells["Id"].Value;
-                row.DefaultCellStyle.BackColor = collisions.Any(n => n.Id == id) ? Color.Red : Color.White;
+                double x = Convert.ToDouble(row.Cells["X"].Value);
+                double y = Convert.ToDouble(row.Cells["Y"].Value);
+                row.DefaultCellStyle.BackColor = collisionSet.Contains((x, y)) ? Color.Gold : Color.White;
             }
-            DrawPipelines(_nodes, collisions.Select(n => n.Id).ToHashSet());
+
+            DrawPipelines(_nodes, collisionSet);
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
@@ -65,34 +87,51 @@ namespace PipelineDesigner
         {
             if (dataGridView1.CurrentRow != null)
             {
-                int id = (int)dataGridView1.CurrentRow.Cells["Id"].Value;
-                _repo.DeleteNode(id);
-                LoadNodes();
+                double x = Convert.ToDouble(dataGridView1.CurrentRow.Cells["X"].Value);
+                double y = Convert.ToDouble(dataGridView1.CurrentRow.Cells["Y"].Value);
+
+                var toDelete = _nodes.FirstOrDefault(n => n.X == x && n.Y == y);
+                if (toDelete != null)
+                {
+                    _repo.DeleteNode(toDelete.Id);
+                    LoadNodes();
+                }
             }
         }
 
-        private void DrawPipelines(List<Node> nodes, HashSet<int> highlight = null)
+        private void DrawPipelines(List<Node> nodes, HashSet<(double X, double Y)> highlight = null)
         {
             var g = panel1.CreateGraphics();
             g.Clear(Color.White);
 
-            var grouped = nodes.GroupBy(n => n.PipelineId);
-            foreach (var group in grouped)
+            var colors = new[] { Pens.Blue, Pens.Orange, Pens.Gray };
+            var brushes = new[] { Brushes.Blue, Brushes.Orange, Brushes.Gray };
+
+            var groups = nodes.GroupBy(n => n.PipelineId).ToList();
+            for (int index = 0; index < groups.Count; index++)
             {
-                var list = group.OrderBy(n => n.Id).ToList();
-                for (int i = 0; i < list.Count - 1; i++)
+                var group = groups[index].OrderBy(n => n.Id).ToList();
+                var pen = colors[index % colors.Length];
+                var brush = brushes[index % brushes.Length];
+
+                for (int i = 0; i < group.Count - 1; i++)
                 {
-                    g.DrawLine(Pens.Blue,
-                        (float)list[i].X * 10, (float)list[i].Y * 10,
-                        (float)list[i + 1].X * 10, (float)list[i + 1].Y * 10);
+                    g.DrawLine(pen,
+                        (float)group[i].X * 40, (float)(300 - group[i].Y * 20),
+                        (float)group[i + 1].X * 40, (float)(300 - group[i + 1].Y * 20));
                 }
 
-                foreach (var n in list)
+                foreach (var node in group)
                 {
-                    Brush brush = highlight != null && highlight.Contains(n.Id) ? Brushes.Red : Brushes.Black;
-                    g.FillEllipse(brush, (float)n.X * 10 - 3, (float)n.Y * 10 - 3, 6, 6);
+                    Brush b = (highlight != null && highlight.Contains((node.X, node.Y))) ? Brushes.Gold : brush;
+                    g.FillEllipse(b, (float)(node.X * 40 - 3), (float)(300 - node.Y * 20 - 3), 6, 6);
                 }
             }
+
+            // легенда
+            g.DrawString("1", new Font("Arial", 8), Brushes.Blue, 500, 20);
+            g.DrawString("2", new Font("Arial", 8), Brushes.Orange, 500, 40);
+            g.DrawString("3", new Font("Arial", 8), Brushes.Gray, 500, 60);
         }
     }
 }
